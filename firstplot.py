de6aeb6c863d95c92a15b49c5f0c5b97f04f9ee3
avmanyc <avmanyc@gmail.com>
2022-12-03 13:20:20 -0500

    able to bring the api key from .config

--- firstplot.py
+++ firstplot.py
@@ -1,6 +1,22 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import configparser, os
 
-x = np.linspace(0, 20, 100)
-plt.plot(x, np.sin(x))
-plt.show()
+config = configparser.ConfigParser()
+config.read_file(open('.gitignore'))
+api = config.api_key
+
+url = 'https://api.adoptapet.com/search/pets_at_shelter?key=A34F48&v=1&output=xml&shelter_id=2342'
+
+
+
+
+# https://app.abstractapi.com/api/holidays/documentation
+
+# print(api)
+
+
+
+# x = np.linspace(0, 20, 100)
+# plt.plot(x, np.sin(x))
+# plt.show()
