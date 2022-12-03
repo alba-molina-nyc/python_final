9ec6ce143df2629058ab16b2bda8cea4b24c0769
avmanyc <avmanyc@gmail.com>
2022-12-03 16:52:07 -0500

    using holidays

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
 # Python Final Project - Binghamton University
 
-This repo seeks to use *matplotlib and numpy* along with the [Holidays Api](https://app.abstractapi.com/api/holidays/documentation) to fetch data and be able to create graphs and charts using the above mentioned libraries.
+This repo seeks to use *matplotlib and numpy* along with the [python holidays library](https://pypi.org/project/holidays/) to fetch data and be able to create graphs and charts using the above mentioned libraries.
 
 ## Technologies
-1. [holidays api documentation](https://holidays.abstractapi.com/v1/)
+1. [python holidays library](https://pypi.org/project/holidays/)
 
     a. api_key in .gitignore file for security purposes
+
+
+### resoures
+1. [geeks for geeks](https://www.geeksforgeeks.org/python-holidays-library/)
 
--- firstplot.py
+++ firstplot.py
@@ -1,16 +1,11 @@
 # import matplotlib.pyplot as plt
 import numpy as np
 import requests
-response = requests.get("https://holidays.abstractapi.com/v1/?api_key=3e9f7a9fdbfe4adda2318bdf3f611928&country=US&year=2020&month=12&day=25")
+from datetime import date
+import holidays
 
-# response = requests.get("https://holidays.abstractapi.com/v1/?api_key=3e9f7a9fdbfe4adda2318bdf3f611928&country=US&year=2020&month=12&day=25")
-print(response.status_code)
-print(response.content)
+north_america = holidays.CA() + holidays.US() + holidays.MX()
+print('north_america: ', north_america)
 
-# https://holidays.abstractapi.com/v1/?api_key=3e9f7a9fdbfe4adda2318bdf3f611928&country=US&year=2020&month=12&day=25 api url
-# print(api_key)
-# https://app.abstractapi.com/api/holidays/documentation the api i am using
-# https://docs.python.org/3/library/configparser.html config parser
-# x = np.linspace(0, 20, 100)
-# plt.plot(x, np.sin(x))
-# plt.show()
+
+# holidays.HolidayBase(years=[], expand=True, observed=True, prov=None, state=None)
