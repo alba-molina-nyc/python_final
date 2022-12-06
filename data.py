c8865a62c4eebad5ac831cb5aba1f65606fa583a
avmanyc <avmanyc@gmail.com>
2022-12-06 08:06:16 -0500

    gets the holidays in the US

--- data.py
+++ data.py
@@ -4,6 +4,17 @@ import numpy as np
 from datetime import date
 
 
-xx = holidays.utils.list_supported_countries()
-print(xx)
+countries = holidays.utils.list_supported_countries()
+for country in countries:
+    if country == 'US':
+        print('country: ', country )
+        print(type(country))
+        print(type(countries))
+        print(type(holidays.US()))
+# print('here: ', all_countries)
+
+
+# US Holidays - 2021
+for day in sorted(holidays.US(years=2023).items()):
+    print(day)
 
