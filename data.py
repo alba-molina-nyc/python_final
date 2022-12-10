151134a02e840a935c65d1972ddb3909d7efebf1
avmanyc <avmanyc@gmail.com>
2022-12-10 11:24:02 -0500

    playing around with the matplot lib

--- data.py
+++ data.py
@@ -8,30 +8,39 @@ from datetime import date
 countries = holidays.utils.list_supported_countries()
 for country in countries:
     if country == 'US':
-        print('country: ', country )
-        print(type(country))
-        print(type(countries))
-        print(type(holidays.US()))
-# print('here: ', all_countries)
+        # print('country: ', country )
+        # print(type(country))
+        # print(type(countries))
+        # print(type(holidays.US()))
+        pass
+# print('here: ', countries)
 
 
 # US Holidays - 2021
 for day in sorted(holidays.US(years=2023).items()):
-    print(day)
+    # print(day)
+    pass
 
 
 # US Holidays - 2021 - CA
 for day, name in sorted(holidays.US(years=2023, state='CA').items()):
-    print(day, name)
+    # print(day, name)
+    pass
 
 
 # check if public holiday or not
 us_holidays = holidays.UnitedStates()
 uk_holidays = holidays.UnitedKingdom()
 fr_holidays = holidays.France()
+
+all_holi = us_holidays + uk_holidays + fr_holidays
 # print('01-01-2022' in us_holidays)
 # print('01-01-2022' in uk_holidays)
 # print('01-01-2022' in fr_holidays)
+
+
+
+
 
 # create holiday list
 
@@ -54,13 +63,42 @@ df.to_csv('/Users/albamolina/files/python_final/ex1_holidays.csv')
 
 # read file
 
-df = pd.read_csv('ex1_holidays.csv')
+# df = pd.read_csv('ex1_holidays.csv')
 
-df.rename(columns = {'Unnamed: 0': 'counter', '0': 'date', '1': 'holiday_name'}, inplace=True) #old name, new name inplace=True -> want ocur in place
+# df.rename(columns = {'Unnamed: 0': 'counter', '0': 'date', '1': 'holiday_name'}, inplace=True) #old name, new name inplace=True -> want ocur in place
 
-plt.plot(df.counter, df.holiday_name)
+# plt.plot(df.counter, df.holiday_name)
+
+# plt.show()
+
+
+# dataset = []
+
+# for item in holidays.UnitedStates(years=yrs).items():
+#     print('item: ',item)
+#     dataset.append({ 'years' : yrs , 'holidays' : item})
+#     print('dataset:', dataset)
+
+dataseta = []
+
+for item in holidays.UnitedStates(years=yrs).items():
+    dataseta.append({'years': yrs, 'holidays': item.count})
+    print('item: ',item)
+    print(type(item))
+    print('dataseta: ',dataseta)
+    # dataseta.append({ 'years' : yrs , 'holidays' : item})
+    # print('dataset:', dataseta)
+
+
+df = pd.DataFrame(dataseta)
+
+df.plot(kind='bar', x='holidays', y='years')
 
 plt.show()
+
+
+
+
 
 # print(df.head)
 # print('columns: ', df.columns)
